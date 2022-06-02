2f3202bedf23b14dbd956e249c4765cc2b432a9a
Levi Eze <somtolevi.eze@gmail.com>
2022-06-02 16:44:19 -0700

    The C Hello, World Task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints the standard error sandwiched around the standard outpu/t
  *
- *  Return: returns 1
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, str1, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
