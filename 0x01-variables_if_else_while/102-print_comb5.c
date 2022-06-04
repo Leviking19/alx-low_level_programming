84824b6b47626ca5e5afa2dcb8f2ec0eaab4973c
Levi Eze <somtolevi.eze@gmail.com>
2022-06-04 14:49:20 -0700

    The C variables,if,else,while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,13 +12,13 @@ int main(void)
 	for (a = 48; a <= 57; a++)
 	{
 	for (b = 48; b <= 57; b++)
-	{  
+	{
 	for (c = 48; c <= 57; c++)
-	{    
+	{
 	for (d = 48; d <= 57; d++)
 	{
 		if (((c + d) > (a + b) && c >= a) || a < c)
-		{      
+		{
 		putchar(a);
 		putchar(b);
 		putchar(' ');
@@ -32,11 +32,11 @@ int main(void)
 		{
 		putchar(',');
 		putchar(' ');
-	        }  
+	        }
 		}
 	}
  	}
-	}   
+	}
         }
         putchar('\n');
         return (0);
