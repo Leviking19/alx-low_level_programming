fd2f25fd9a1d38eb716d1bd4521931c496c7b19c
Levi Eze <somtolevi.eze@gmail.com>
2022-06-03 22:11:38 -0700

    The C Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,20 +11,23 @@ int main(void)
 
 	for (x = '0'; x <= '9'; x++)
 	{
-		if (x < y)
+		for (y = '0'; y <= '9'; y++)
 		{
-			putchar(x);
-			putchar(y);
-
-			if (x != '8' || (x == '8' && y != '9'))
+			if (x < y)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(x);
+				putchar(y);
+
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-}
-putchar('\n');
 
-return (0);
+	putchar('\n');
+
+	return (0);
 }
