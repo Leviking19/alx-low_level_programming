dcfb00016af774bca05741b5d5e386373e4fb940
Levi Eze <somtolevi.eze@gmail.com>
2022-06-14 09:30:35 -0700

    The C pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap between a and b
+ * @l: L pointer to an int that will be updated
+ * @m: M pointer to an int that will be updated
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int z; /*Declaring variables*/
+
+	z = *l;  /*Z value l*/
+	*l = *m;  /*l value of m*/
+	*m = z; /*m value of Z*/
+}
